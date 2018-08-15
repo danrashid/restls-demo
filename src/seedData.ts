4ae6a2c89f0582983967bb1d13aa54e50163b135
Dan Rashid <dan@pecsafety.com>
2018-08-15 09:37:00 -0500

    Use consts for collection names

--- src/seedData.ts
+++ src/seedData.ts
@@ -1,9 +1,10 @@
 import * as fakeData from './fakeData';
+import { companies, employees, users } from './interfaces/collections';
 import { getCollection, setCollection } from 'restls';
 
 export default () => {
   try {
-    ["companies", "employees", "users"].every(getCollection);
+    [companies, employees, users].every(getCollection);
   } catch (e) {
     const data = fakeData();
 
